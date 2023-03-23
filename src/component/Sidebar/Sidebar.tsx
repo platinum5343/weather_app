1db86508d77a2ae2f284f1e369512f848f4c8b86
platinum5343 <olatunjiolamiji06@gmail.com>
2023-03-24 01:14:10 +1400

    first commit

---
+++ src/component/Sidebar/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Sidebar.css';
+
+
+const Sidebar = () => {
+  return (
+      <div className='container'>
+        <div className="sidebar">
+            <div className="topsection">
+                <div className="bars"></div>
+        
+        </div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+    <div>Sidebar</div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
